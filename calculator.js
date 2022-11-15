488d92a94f9612a29203c13fd7fab0b46af14cf4
OpeDada <opedada@gmail.com>
2022-11-15 16:19:00 +0900

    populate handle number function to update on calc screen

--- calculator.js
+++ calculator.js
@@ -1,12 +1,27 @@
 let runningTotal = 0;
-let buffer = "0";
+let buffer = "0"; // starting figure on the calculator
 let previousOperator;
 
 const screen = document.querySelector(".screen");
 
-function buttonClick(value) {}
+function buttonClick(value) {
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+}
 
+function handleSymbol(symbol) {}
+function handleNumber(numberString) {
+  if (buffer === "0") {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
 
+  screen.innerText = buffer;
+}
 
 function init() {
   document
